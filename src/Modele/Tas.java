60b6541507f80dfe590971c7b8615dfcf7d74eed
estelar9 <esteban.draily.pro@gmail.com>
2024-03-06 18:21:32 +0100

    initialisation2 du projet

--- src/Modele/Tas.java
+++ src/Modele/Tas.java
@@ -1,4 +1,25 @@
 package Modele;
 
 public class Tas {
+    private int numTas;
+    private int allumette;
+
+    public Tas(int numTas){
+        this.numTas=numTas;
+        allumette=numTas*2-1;
+    }
+
+    public boolean estVide(){
+        return numTas<=0;
+    }
+
+    public boolean supprimerAllumettes(int nbAllumette){
+        if (nbAllumette>allumette){
+            return false;
+        } else {
+            allumette=allumette-nbAllumette;
+            return true;
+        }
+    }
+
 }
