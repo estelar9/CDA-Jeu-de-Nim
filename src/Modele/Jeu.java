b124ea68e8cd2ffbc3966ef8dc47205666d95160
Erasmatu <erasmatu241@gmail.com>
2024-03-07 14:28:09 +0100

    Creation du constructeur et des getter et setter dans le classe Jouer .Mise au point du map les jouer et initialisation du score à 0 .

--- src/Modele/Jeu.java
+++ src/Modele/Jeu.java
@@ -1,15 +1,24 @@
 package Modele;
 
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class Jeu {
     private Set<Partie> lesParties;
+    private Map<Joueur,Integer>lesJoueurs;
     public Jeu(int nbTas){
         lesParties=new TreeSet<>();
         lesParties.add(creerPartie(nbTas));
+
     }
     public Partie creerPartie(int nbTas){
         return new Partie(nbTas);
+    }
+
+    public boolean CreeJoueur(String nom){
+        Joueur nouveauJoueur = new Joueur(nom);
+        lesJoueurs.put(nouveauJoueur,0);
+        return true;
     }
 }
--- src/Modele/Joueur.java
+++ src/Modele/Joueur.java
@@ -1,4 +1,29 @@
 package Modele;
 
 public class Joueur {
+    private static int denierJoueur = 0 ;
+
+    private int numJoueur ;
+    private String nomJoueur;
+
+    public Joueur(String nomJouer) {
+        this.nomJoueur = nomJoueur;
+        this.numJoueur = ++denierJoueur;
+    }
+
+    public int getNumJoueur() {
+        return numJoueur;
+    }
+
+    public void setNumJoueur(int numJoueur) {
+        this.numJoueur = numJoueur;
+    }
+
+    public String getNomJouer() {
+        return nomJoueur;
+    }
+
+    public void setNomJouer(String nomJouer) {
+        this.nomJoueur = nomJouer;
+    }
 }
