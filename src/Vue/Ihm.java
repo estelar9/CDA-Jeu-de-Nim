35719989b09fcacf3cc5c2cf17f17dce7eef03be
estelar9 <esteban.draily.pro@gmail.com>
2024-03-07 12:09:12 +0100

    préecriture des fonctions dans l'ihm

--- src/Vue/Ihm.java
+++ src/Vue/Ihm.java
@@ -4,4 +4,12 @@ public class Ihm {
     public void affichage(String texte){
         System.out.println(texte);
     }
+    public int ihmDemanderNbTas(){}
+    public String ihmDemanderNomJoueur(){}
+    public String ihmDemanderCoup(){
+        //retourne le string d'un coup de forme "m n" on recuperera dans le controleur les int
+    }
+    public String ihmRejouer(){
+        // retourne un string genre "y" "o" "yes" "oui" ou "n" "no" etc on traitera le resultat dans le controleur
+    }
 }
