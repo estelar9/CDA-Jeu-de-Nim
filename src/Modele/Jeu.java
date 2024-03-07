9f3d4405c126c9d3400c4e6052c26e1a9daa9a76
estelar9 <esteban.draily.pro@gmail.com>
2024-03-07 14:23:09 +0100

    avancement global

--- src/Modele/Jeu.java
+++ src/Modele/Jeu.java
@@ -5,13 +5,18 @@ import java.util.TreeSet;
 import java.util.Map ;
 
 public class Jeu {
+    private Partie partieEnCours;
     private Partie partiesEnCours ;
     private Map<Joueur,Integer> lesJoueurs ;
     public Jeu(int nbTas){
-        partiesEnCours = new Partie(nbTas);
+        partieEnCours=new Partie(nbTas);
     }
-    public Partie creerPartie(int nbTas){
-        return new Partie(nbTas);
+    public void nouvellePartie(int nbTas){
+        partieEnCours=new Partie(nbTas);
+    }
+
+    public boolean joueurSuivant(){
+
     }
     public boolean jouerCoup (int m , int n ){
         return partiesEnCours.enleverAllumettes(m,n);
