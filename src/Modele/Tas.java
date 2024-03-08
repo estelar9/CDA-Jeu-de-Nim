f88fe639976ade97720715f57ae2a57540ce43cd
hild365 <khild.gomado@gmail.com>
2024-03-08 17:53:21 +0100

    complétion

--- src/Modele/Tas.java
+++ src/Modele/Tas.java
@@ -16,6 +16,8 @@ public class Tas implements Comparable<Tas> {
     public void supprimerAllumettes(int nbAllumettes) throws WrongInputException {
         if (nbAllumettes>allumette){
             throw new WrongInputException("Oups ! Pas assez d'allumettes dans le tas "+"\u001B[33m"+numTas+"\u001B[0m");
+        } else if (nbAllumettes<=0) {
+            throw new WrongInputException("Zut ! le nombre d'allumettes doit être supérieur à 0 ");
         } else {
             allumette=allumette-nbAllumettes;
         }
